54482fab7a42b427a1a39a61bc354fa85595e1ff
Augusto Becciu <augusto@becciu.org>
2014-05-01 23:15:15 -0300

    Fixed linux's type conversion functions.

--- util_linux.go
+++ util_linux.go
@@ -6,9 +6,9 @@ package jlog
 import "C"
 
 func float64ToTimeT(num float64) C.__time_t {
-	return C.__darwin_time_t(num)
+	return C.__time_t(num)
 }
 
-func int64ToSusecondsT(numint64) C.__suseconds_t {
-	return C.__darwin_suseconds_t(num)
+func int64ToSusecondsT(num int64) C.__suseconds_t {
+	return C.__suseconds_t(num)
 }
